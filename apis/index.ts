3a43beb5de03542ca8c9707b0513c35e5ed0635b
Lucas Carvalho <lvdcarvalho92@gmail.com>
2024-10-10 00:12:29 +0800

    client v0.1

---
+++ apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DefaultApi';
+export * from './FloatingIpApi';
+export * from './VmApi';
+export * from './VpcApi';
+export * from './WhmcsApi';
---
+++ dist/apis/index.d.ts
@@ -0,0 +1,5 @@
+export * from './DefaultApi';
+export * from './FloatingIpApi';
+export * from './VmApi';
+export * from './VpcApi';
+export * from './WhmcsApi';
